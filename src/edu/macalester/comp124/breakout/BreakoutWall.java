c7553e497e8642064963f3286dded30d462d8694
Ye Vang <yvang@macalester.edu>
2015-03-11 23:48:17 -0500

    Wall of bricks with some overhead space for a 'breakout'.

--- src/edu/macalester/comp124/breakout/BreakoutWall.java
+++ src/edu/macalester/comp124/breakout/BreakoutWall.java
@@ -14,19 +14,19 @@ public class BreakoutWall extends GCompound {
         for (int i = 0; i < brick.getBrick().getWidth()* 10; i += brick.getBrick().getWidth()) {
             for (int j = 0; j < brick.getBrick().getHeight() * 2; j += brick.getBrick().getHeight()) {
                 Brick redBrick = new Brick(Color.RED);
-                add(redBrick.getBrick(), i, j);
+                add(redBrick.getBrick(), i, j + 100);
 
                 Brick orangeBrick = new Brick(Color.ORANGE);
-                add(orangeBrick.getBrick(), i, j + orangeBrick.getBrick().getHeight()*2);
+                add(orangeBrick.getBrick(), i, j + orangeBrick.getBrick().getHeight()*2 + 100);
 
                 Brick yellowBrick = new Brick(Color.YELLOW);
-                add(yellowBrick.getBrick(), i, j + yellowBrick.getBrick().getHeight()*4);
+                add(yellowBrick.getBrick(), i, j + yellowBrick.getBrick().getHeight()*4 + 100);
 
                 Brick greenBrick = new Brick(Color.GREEN);
-                add(greenBrick.getBrick(), i, j + greenBrick.getBrick().getHeight()*6);
+                add(greenBrick.getBrick(), i, j + greenBrick.getBrick().getHeight()*6 + 100);
 
                 Brick cyanBrick = new Brick(Color.CYAN);
-                add(cyanBrick.getBrick(), i, j + cyanBrick.getBrick().getHeight()*8);
+                add(cyanBrick.getBrick(), i, j + cyanBrick.getBrick().getHeight()*8 + 100);
             }
         }
     }
