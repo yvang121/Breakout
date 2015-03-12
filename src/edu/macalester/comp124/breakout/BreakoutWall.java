1c87ed24aaf494097d5fd22ca13898b3f04500b1
Ye Vang <yvang@macalester.edu>
2015-03-12 11:00:15 -0500

    Brick wall containing difference colored bricks in a row.

--- src/edu/macalester/comp124/breakout/BreakoutWall.java
+++ src/edu/macalester/comp124/breakout/BreakoutWall.java
@@ -14,19 +14,19 @@ public class BreakoutWall extends GCompound {
         for (int i = 0; i < brick.getBrick().getWidth()* 10; i += brick.getBrick().getWidth()) {
             for (int j = 0; j < brick.getBrick().getHeight() * 2; j += brick.getBrick().getHeight()) {
                 Brick redBrick = new Brick(Color.RED);
-                add(redBrick.getBrick(), i, j + 150);
+                add(redBrick.getBrick(), i, j + 125);
 
                 Brick orangeBrick = new Brick(Color.ORANGE);
-                add(orangeBrick.getBrick(), i, j + orangeBrick.getBrick().getHeight()*2 + 150);
+                add(orangeBrick.getBrick(), i, j + orangeBrick.getBrick().getHeight()*2 + 125);
 
                 Brick yellowBrick = new Brick(Color.YELLOW);
-                add(yellowBrick.getBrick(), i, j + yellowBrick.getBrick().getHeight()*4 + 150);
+                add(yellowBrick.getBrick(), i, j + yellowBrick.getBrick().getHeight()*4 + 125);
 
                 Brick greenBrick = new Brick(Color.GREEN);
-                add(greenBrick.getBrick(), i, j + greenBrick.getBrick().getHeight()*6 + 150);
+                add(greenBrick.getBrick(), i, j + greenBrick.getBrick().getHeight()*6 + 125);
 
                 Brick cyanBrick = new Brick(Color.CYAN);
-                add(cyanBrick.getBrick(), i, j + cyanBrick.getBrick().getHeight()*8 + 150);
+                add(cyanBrick.getBrick(), i, j + cyanBrick.getBrick().getHeight()*8 + 125);
             }
         }
     }
